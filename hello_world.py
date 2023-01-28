e3f96e5f75908d0a4958b7db2b19633ada886561
ewaaaem <ewa.malecha97@gmail.com>
2023-01-28 14:43:15 +0100

    Hello world

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+# Tutaj pisz swój kod, młody padawanie ;-)
+print('Hello World')
+print ("Hello World2")
+
+a=5
+b=7.5
+c='Diana'
+
+print(a)
+print(b)
+print(c)
+
+suma = a+b
+print (suma)
+print (a+b)
+print (a*c)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(suma))
