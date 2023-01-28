9e181b919630caa0b9a47855f705ccdb89c064dc
ewaaaem <ewa.malecha97@gmail.com>
2023-01-28 14:44:19 +0100

    input

---
+++ input.py
@@ -0,0 +1,12 @@
+# Tutaj pisz swój kod, młody padawanie ;-)
+#dane = input('Wpisz imię, nazwisko i wiek, oddzielając gwiazdką').split('*')
+#print('Cześć', dane[0],'. Masz', dane[2], 'lat')
+
+#dane = input('Wpisz imię, nazwisko i wiek, oddzielając spacją').split()
+#print('Cześć', dane[0],'. Masz', dane[2], 'lat')
+
+wyplata = int(input('Wpisz wypłatę'))
+if wyplata <= 5000:
+    print('Gratulacje')
+    print('Wysłanie maila')
+print('Kolejna instrukcja')
