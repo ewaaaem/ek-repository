e920d48435d83274b8dd69d02bed720e0be5e8e6
ewaaaem <ewa.malecha97@gmail.com>
2023-01-28 14:44:47 +0100

    lista

---
+++ lista.py
@@ -0,0 +1,19 @@
+# Tutaj pisz swój kod, młody padawanie ;-)
+a=5
+b='Diana'
+lista=[5, 7, 12.5, 18, 26, 'Ewa', 'Diana', 32]
+print (lista)
+print(lista[3])
+print (lista[-1])
+#jak wpiszę lista[8] to nie wyświetli, bo nie ma takiego elementu, a ostatni element to lista[-1] lub lista[7]
+
+dane ='mama  tata pies kot'
+nowe_dane=dane.split() #jak jest pusty nawias to rozdziela po spacji
+print (nowe_dane)
+print (nowe_dane[-1])
+
+
+dane2 = 'mama.tata.ja.ty'
+dane2=dane2.split('.') #rozdziela po kropce
+print(dane2)
+print(dane2[2])
